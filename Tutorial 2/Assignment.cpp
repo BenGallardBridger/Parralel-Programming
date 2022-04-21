f11bb7a9297bd699437819f9a252621197bdd161
BenGallardBridger <60215170+BenGallardBridger@users.noreply.github.com>
2022-04-21 17:00:29 +0100

    Update

--- Tutorial 2/Assignment.cpp
+++ Tutorial 2/Assignment.cpp
@@ -13,6 +13,7 @@ void print_help() {
 	std::cerr << "  -d : select device" << std::endl;
 	std::cerr << "  -l : list all platforms and devices" << std::endl;
 	std::cerr << "  -f : input image file (default: test.pgm)" << std::endl;
+	std::cerr << "  -b : bin size (default: 128)" << std::endl;
 	std::cerr << "  -h : print this message" << std::endl;
 }
 
@@ -21,12 +22,13 @@ int main(int argc, char** argv) {
 	int platform_id = 0;
 	int device_id = 0;
 	string image_filename = "test.pgm";
-
+	int bin_size = 128;
 	for (int i = 1; i < argc; i++) {
 		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
 		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
 		else if (strcmp(argv[i], "-l") == 0) { std::cout << ListPlatformsDevices() << std::endl; }
 		else if ((strcmp(argv[i], "-f") == 0) && (i < (argc - 1))) { image_filename = argv[++i]; }
+		else if ((strcmp(argv[i], "-b") == 0) && (i < (argc - 1))) { bin_size = atoi(argv[++i]); }
 		else if (strcmp(argv[i], "-h") == 0) { print_help(); return 0; }
 	}
 
@@ -34,7 +36,7 @@ int main(int argc, char** argv) {
 
 	//detect any potential exceptions
 	try {
-		CImg<unsigned short> image_input;
+		CImg<unsigned char> image_input;
 		image_input.load(image_filename.c_str());
 
 		CImgDisplay disp_input(image_input, "input");
@@ -68,11 +70,10 @@ int main(int argc, char** argv) {
 		}
 
 		//Part 4 - device operations
-		int max = ((int)image_input.max()) + 2;
-		cerr << max << "\n";
-		size_t vector_elements = 128;//number of elements
-		size_t vector_size = 128 * sizeof(unsigned int);//size in bytes
-
+		size_t vector_elements = bin_size;//number of elements
+		size_t vector_size = bin_size * sizeof(unsigned int);//size in bytes
+		int max = image_input.max();
+		std::cout << max << std::endl;
 		//host - output
 		std::vector<unsigned int> C(vector_elements);
 
@@ -83,60 +84,67 @@ int main(int argc, char** argv) {
 
 		cl::Buffer buffer_D(context, CL_MEM_READ_WRITE, vector_size);
 
-		size_t vector_size1 = max * sizeof(unsigned short);//size in bytes
+		size_t vector_size1 = bin_size * sizeof(unsigned short);//size in bytes
 
-		std::vector<unsigned short> E(vector_elements);
+		std::vector<unsigned int> E(vector_elements);
 
-		cl::Buffer buffer_E(context, CL_MEM_READ_WRITE, vector_size1);
+		cl::Buffer buffer_E(context, CL_MEM_READ_WRITE, vector_size);
 
 		//device - buffers
 		cl::Buffer dev_image_input(context, CL_MEM_READ_ONLY, image_input.size());
 		cl::Buffer dev_image_output(context, CL_MEM_READ_WRITE, image_input.size()); //should be the same as input image
-
+		cl::Buffer numOfBins(context, CL_MEM_READ_ONLY, sizeof(int));
+		cl::Buffer maximumValue(context, CL_MEM_READ_ONLY, sizeof(int));
 		//4.1 Copy images to device memory
 		queue.enqueueWriteBuffer(dev_image_input, CL_TRUE, 0, image_input.size(), &image_input.data()[0]);
+		queue.enqueueWriteBuffer(numOfBins, CL_TRUE, 0, sizeof(int), &bin_size);
+		queue.enqueueWriteBuffer(maximumValue, CL_TRUE, 0, sizeof(int), &max);
 
 
 		//4.2 Setup and execute the kernel (i.e. device code)
 		cl::Kernel kernel = cl::Kernel(program, "histogramVals");
 		kernel.setArg(0, dev_image_input);
-		kernel.setArg(1, buffer_C);
+		kernel.setArg(1, numOfBins);
+		kernel.setArg(2, maximumValue);
+		kernel.setArg(3, buffer_C);
 		
-		cl::Kernel kernelB = cl::Kernel(program, "cumHistogramVals");
+		cl::Kernel kernelB = cl::Kernel(program, "scan_hs");
 		kernelB.setArg(0, buffer_C);
 		kernelB.setArg(1, buffer_D);
 
 		cl::Kernel kernelC = cl::Kernel(program, "normHistogramVals");
 		kernelC.setArg(0, buffer_D);
-		kernelC.setArg(1, buffer_E);
+		kernelC.setArg(1, maximumValue);
+		kernelC.setArg(2, buffer_E);
 
 		cl::Kernel kernelD = cl::Kernel(program, "mapHistogram");
 		kernelD.setArg(0, dev_image_input);
 		kernelD.setArg(1, buffer_E);
-		kernelD.setArg(2, dev_image_output);
+		kernelD.setArg(2, maximumValue);
+		kernelD.setArg(3, numOfBins);
+		kernelD.setArg(4, dev_image_output);
 
 		queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange);
-		queue.enqueueNDRangeKernel(kernelB, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange);
 		queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, vector_size, &C[0]);
 		cerr << "C = " << C;
+		queue.enqueueNDRangeKernel(kernelB, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange);
 		queue.enqueueNDRangeKernel(kernelC, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange);
 		queue.enqueueReadBuffer(buffer_D, CL_TRUE, 0, vector_size, &D[0]);
 		cerr << "\nD = " << D;
 		queue.enqueueNDRangeKernel(kernelD, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange);
-		cerr << "ENQUE";
-		queue.enqueueReadBuffer(buffer_E, CL_TRUE, 0, vector_size1, &E[0]);
+		queue.enqueueReadBuffer(buffer_E, CL_TRUE, 0, vector_size, &E[0]);
 		cerr << "\nE = " << E;
 		
-		vector<unsigned short> output_buffer(image_input.size());
+		vector<unsigned char> output_buffer(image_input.size());
 		//4.3 Copy the result from device to host
 		queue.enqueueReadBuffer(dev_image_output, CL_TRUE, 0, output_buffer.size(), &output_buffer.data()[0]);
 
-		CImg<unsigned short> output_image(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
+		CImg<unsigned char> output_image(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
 		CImgDisplay disp_output(output_image, "output");
 		
 
-		while (!disp_output.is_closed() && !disp_input.is_closed()
-			&& !disp_output.is_keyESC() && !disp_input.is_closed()) {
+		while (!disp_input.is_closed() && !disp_output.is_closed()
+			&& !disp_input.is_keyESC() && !disp_output.is_keyESC()) {
 			disp_input.wait(1);
 			disp_output.wait(1);
 		}
--- Tutorial 2/kernels/my_kernels.cl
+++ Tutorial 2/kernels/my_kernels.cl
@@ -1,8 +1,27 @@
 //converts values into histogram
-kernel void histogramVals(global const ushort* A, global uint* B) {
+kernel void histogramVals(global const uchar* A, global const int* binSize, global const int* maximum, global uint* B) {
 	int id = get_global_id(0);
-	int bin_num = (A[id]/65537.f)*127;
+	int bin_num = (A[id]/(float)maximum[0])*binSize[0]-1;
+
 	atomic_inc(&B[bin_num]);
+}
+
+//Hillis-Steele basic inclusive scan
+//requires additional buffer B to avoid data overwrite 
+kernel void scan_hs(global uint* A, global uint* B) {
+	int id = get_global_id(0);
+	int N = get_global_size(0);
+	global uint* C;
+
+	for (int stride = 1; stride < N; stride *= 2) {
+		B[id] = A[id];
+		if (id >= stride)
+			B[id] += A[id - stride];
+
+		barrier(CLK_GLOBAL_MEM_FENCE); //sync the step
+
+		C = A; A = B; B = C; //swap A & B between steps
+	}
 }
 
 kernel void cumHistogramVals(global const uint* A, global uint* B) {
@@ -15,21 +34,22 @@ kernel void cumHistogramVals(global const uint* A, global uint* B) {
 	B[id] = temp;
 }
 
-kernel void normHistogramVals(global const uint* A, global ushort* B) {
+kernel void normHistogramVals(global const uint* A, global const int* maximum, global int* B) {
 	int id = get_global_id(0);
 	int size = get_global_size(0);
 	int max = A[size - 1];
-	int temp = ((double)A[id] / (double)max) * (double)size;
-	if (temp > size - 1) {
+	int temp = (A[id] / (float)max) * maximum[0];
+	if (temp > maximum[0]) {
 
-		B[id] = (ushort)(size - 1);
+		B[id] = (int)(maximum[0]);
 	}
 	else {
-		B[id] = (ushort)temp;
+		B[id] = (int)temp;
 	}
 }
 
-kernel void mapHistogram(global const ushort* A, global const ushort* B, global ushort* C) {
+kernel void mapHistogram(global const uchar* A, global const int* B, global const int* maximum, global const int* binSize, global uchar* C) {
 	int id = get_global_id(0);
-	C[id] = B[A[id]];
+	int binNum = (A[id] / (float)maximum[0]) * binSize[0];
+	C[id] = B[binNum];
 }
